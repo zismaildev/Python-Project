e12b5d979db4400acb4b69558ed59b0f9333e2f3
Nattapong Panthiya <nattapong130247@gmail.com>
2023-09-25 21:29:03 +0700

    update

--- zismail.py
+++ zismail.py
@@ -1,11 +1,24 @@
-print("Zismail Program")
-print("")
+print("Zismail-Dev")
+print("Savings calculation program")
 
-amount = float(input("ป้อนจำนวนเงินที่ต้องการออม: "))
-percent = int(input("เลือกเปอร์เซ็นต์ที่ต้องการออม (50, 30, 20, หรือ 10): "))
+while True:
+    savings = float(input("Enter the starting amount. : "))
 
-if percent in [50, 30, 20, 10]:
-    savings = (amount * percent) / 100
-    print(f"คุณจะออมได้ {savings:.2f}")
-else:
-    print("เปอร์เซ็นต์ไม่ถูกต้อง โปรดเลือก 50, 30, 20, หรือ 10.")
+    while savings > 0.10: 
+        print(f"Balance: {savings:.2f}")
+
+        percent = int(input("Choose the percentage you want to save. (50, 30, 20, 10): "))
+        if percent not in [50, 30, 20, 10]:
+            print("Incorrect percentage. Please select. 50, 30, 20, or 10")
+            continue
+
+        amount_to_save = (savings * percent) / 100
+        savings -= amount_to_save
+
+        print(f"Saving amount: {amount_to_save:.2f}")
+        print(f"Balance: {savings:.2f}")
+
+        another_round = input("Want to find more? (y/n): ")
+        if another_round.lower() != 'y':
+            print("Thank you for using the service. Goodbye program")
+            break
