a39c1bad16c4be06fae05bd408892b7b707a30a0
Nattapong Panthiya <nattapong130247@gmail.com>
2023-09-27 01:04:57 +0700

    Zismail Calulater Ptogram

--- zismail.py
+++ zismail.py
@@ -1,24 +1,25 @@
-print("Zismail-Dev")
 print("Savings calculation program")
+print("Zismail-Dev")
+print("Program for Zismail")
 
 while True:
-    savings = float(input("Enter the starting amount. : "))
+    money = float(input("กรุณาใส่จำนวนเงิน (บาท) : "))
+    percent = int(input("กรุณาใส่สัดส่วนการออม (เปอร์เซนต์) (50, 30, 20, 10) : "))
 
-    while savings > 0.10: 
-        print(f"Balance: {savings:.2f}")
+    # คำนวณค่าใช้จ่าย, เงินลงทุน, เงินที่ใช้ไม่ได้, และเงินที่เหลือสำหรับใช้ได้
+    earn = (money * percent) /100
+    expenses = earn * (percent / 100)
+    investment = earn * 0.2
+    unusable_money = earn * 0.3
+    balance = money - earn
 
-        percent = int(input("Choose the percentage you want to save. (50, 30, 20, 10): "))
-        if percent not in [50, 30, 20, 10]:
-            print("Incorrect percentage. Please select. 50, 30, 20, or 10")
-            continue
-
-        amount_to_save = (savings * percent) / 100
-        savings -= amount_to_save
-
-        print(f"Saving amount: {amount_to_save:.2f}")
-        print(f"Balance: {savings:.2f}")
-
-        another_round = input("Want to find more? (y/n): ")
-        if another_round.lower() != 'y':
-            print("Thank you for using the service. Goodbye program")
-            break
+    # แสดงผลลัพธ์
+    print(f"ค่าใช้จ่าย (expenses): {expenses:.2f} บาท")
+    print(f"เงินลงทุน (investment): {investment:.2f} บาท")
+    print(f"เงินที่ใช้ไม่ได้ (unusable_money): {unusable_money:.2f} บาท")
+    print(f"เงินที่เหลือสำหรับใช้ได้ (remaining_money): {balance:.2f} บาท")
+    
+    another_round = input("ต้องการคำนวณเพิ่มเติมหรือไม่? (y/n): ")
+    if another_round.lower() != 'y':
+        print("ขอบคุณที่ใช้บริการครับ ลาก่อย")
+        break
