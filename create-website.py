368fd3bfdf4054da87a6d8bb43e146775f616df0
zismaildev <nattapong130247@gmail.com>
2023-12-15 01:00:34 +0700

    Update create-website.py

--- create-website.py
+++ create-website.py
@@ -25,36 +25,62 @@ def get_project_name():
 def setup_nextjs(project_name):
     subprocess.run(["cmd", "/c", "pnpx create-next-app", project_name])
     os.chdir(project_name)  # Change directory in Python script
+
+    # Create 'components' folder in 'my-app\src'
+    os.makedirs("src/components", exist_ok=True)
+
+    # Create 'contents' folder in 'my-app\src\pages'
+    os.makedirs("src/pages/contents", exist_ok=True)
+
     subprocess.run(["cmd", "/c", "pnpm", "add", "@nextui-org/theme", "@nextui-org/system", "framer-motion"])
-    
+
     with open(".npmrc", "w") as npmrc_file:
         npmrc_file.write("public-hoist-pattern[]=*@nextui-org/*\n")
-    
+
     with open("tailwind.config.js", "w") as tailwind_config_file:
         tailwind_config_file.write("""
-import { nextui } from "@nextui-org/react";
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  darkMode: "class",
-  plugins: [nextui()],
-};
-""")
+        import { nextui } from "@nextui-org/react";
+        /** @type {import('tailwindcss').Config} */
+        module.exports = {
+        content: [
+            "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+            "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+            "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+            "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+        ],
+        theme: {
+            extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            },
+        },
+        darkMode: "class",
+        plugins: [nextui()],
+        };
+        """)
     
-    print("Next.js project setup completed successfully!")
+    # Add the import statement to _app.js
+    # with open("src/pages/_app.js", "w") as file:
+    #     file.write("""
+    #     // Components
+    #     import '@/styles/globals.css'
+    #     import { NextUIProvider } from '@nextui-org/react'
+    #     import Layout from '@/components/layout'
+
+    #     export default function App({ Component, pageProps }) {
+    #     return (
+    #         <NextUIProvider>
+    #         <Layout>
+    #             <Component {...pageProps} />
+    #         </Layout>
+    #         </NextUIProvider>
+    #     )
+    #     }
+    #     """)
+
+        print("Next.js project setup completed successfully!")
 
 def setup_vite(project_name):
     subprocess.run(["cmd", "/c", "pnpm", "init", "vite@latest", project_name, "-e", "vue"])
